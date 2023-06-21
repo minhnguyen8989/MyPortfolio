737175dfa483add35df04fbbb61c81a74901ff16
Minh Nguyen <122935530+minhnguyen8989@users.noreply.github.com>
2023-06-21 15:35:33 -0500

    porfolio updated with about me and contact

--- js/main.js
+++ js/main.js
@@ -1,83 +1,14 @@
 
-function generateDropdownMarkup(container) {
-    const $navWrapper = container.find(".nav-wrapper");
-    const $navPills = container.find(".nav-pills");
-    const firstTextLink = $navPills.find("li:first-child a").text();
-    const $items = $navPills.find("li");
-    const markup = `
-    <div class="dropdown d-md-none">
-      <button class="btn dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        ${firstTextLink}
-      </button>
-      <div class="dropdown-menu" aria-labelledby="dropdownMenuButton"> 
-        ${generateDropdownLinksMarkup($items)}
-      </div>
-    </div>
-  `;
-    $navWrapper.prepend(markup);
-}
+let contactBtn = document.getElementById("contactBtn")
+contactBtn.addEventListener("click", function () {
+    let greeting = "<h5>Hello! One click contact me</h5>"
+    let phoneNumber = "<a href='tel:+3465678989'><i class=\"fa-solid fa-phone mr-2\"></i>(346)-567-8989</a>";
+    let emailAddress = "<a href='mailto:quangminh.nguyen8989@gmail.com'><i class=\"fa-solid fa-envelope mr-2\"></i>quangminh.nguyen8989@gmail.com</a>";
+    $("#contactText").html(greeting + phoneNumber + emailAddress);
+})
 
-function generateDropdownLinksMarkup(items) {
-    let markup = "";
-    items.each(function () {
-        const textLink = $(this).find("a").text();
-        markup += `<a class="dropdown-item" href="#">${textLink}</a>`;
-    });
-
-    return markup;
-}
-
-function showDropdownHandler(e) {
-    // works also
-    //const $this = $(this);
-    const $this = $(e.target);
-    const $dropdownToggle = $this.find(".dropdown-toggle");
-    const dropdownToggleText = $dropdownToggle.text().trim();
-    const $dropdownMenuLinks = $this.find(".dropdown-menu a");
-    const dNoneClass = "d-none";
-    $dropdownMenuLinks.each(function () {
-        const $this = $(this);
-        if ($this.text() == dropdownToggleText) {
-            $this.addClass(dNoneClass);
-        } else {
-            $this.removeClass(dNoneClass);
-        }
-    });
-}
-
-function clickHandler(e) {
-    e.preventDefault();
-    const $this = $(this);
-    const index = $this.index();
-    const text = $this.text();
-    $this.closest(".dropdown").find(".dropdown-toggle").text(`${text}`);
-    $this
-        .closest($tabsToDropdown)
-        .find(`.nav-pills li:eq(${index}) a`)
-        .tab("show");
-}
-
-function shownTabsHandler(e) {
-    // works also
-    //const $this = $(this);
-    const $this = $(e.target);
-    const index = $this.parent().index();
-    const $parent = $this.closest($tabsToDropdown);
-    const $targetDropdownLink = $parent.find(".dropdown-menu a").eq(index);
-    const targetDropdownLinkText = $targetDropdownLink.text();
-    $parent.find(".dropdown-toggle").text(targetDropdownLinkText);
-}
-
-$tabsToDropdown.each(function () {
-    const $this = $(this);
-    const $pills = $this.find('a[data-toggle="pill"]');
-
-    generateDropdownMarkup($this);
-
-    const $dropdown = $this.find(".dropdown");
-    const $dropdownLinks = $this.find(".dropdown-menu a");
-
-    $dropdown.on("show.bs.dropdown", showDropdownHandler);
-    $dropdownLinks.on("click", clickHandler);
-    $pills.on("shown.bs.tab", shownTabsHandler);
-});
+let aboutmeBtn = document.getElementById("aboutMe")
+aboutmeBtn.addEventListener("click", function () {
+    let aboutMe = "<p>As a passionate and dedicated web developer, I thrive on the exciting challenges that come with crafting innovative digital experiences. Armed with a meticulous attention to detail and an extensive knowledge of coding languages and frameworks, I have refined my abilities to fashion visually captivating and exceptionally functional websites. I am excited to continue pushing boundaries, embracing new technologies, and delivering exceptional web solutions that leave a lasting impression.</p>"
+    $("#contactText").html(aboutMe);
+})
